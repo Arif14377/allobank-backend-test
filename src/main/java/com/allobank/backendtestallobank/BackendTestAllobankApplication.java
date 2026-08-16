21d421d27350f37585ee4117c351ddaa2b718406
Arif Rahman <arifrahman14377@gmail.com>
2026-08-16 16:54:18 +0700

    init: initial springboot project for allobank backend test

---
+++ src/main/java/com/allobank/backendtestallobank/BackendTestAllobankApplication.java
@@ -0,0 +1,13 @@
+package com.allobank.backendtestallobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestAllobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTestAllobankApplication.class, args);
+	}
+
+}
